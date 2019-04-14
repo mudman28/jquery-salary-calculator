bb2f1c7cde69ed59bb11b6fc7a7e0ba6721a721e
mudman28 <johnnie.walters@gmail.com>
2019-04-14 09:06:59 -0500

    the calculator is working

--- client.js
+++ client.js
@@ -1,20 +1,15 @@
 $( document ).ready( readyNow );
 
 let employeeInfo = []
-let wages = 0;
+let totalAmount = 0;
+let monthlyAmount = 0;
 
+//click function
 function readyNow(){
     $( '#submitButton' ).on( 'click', submitEEInfo );
 }
 
-function readyToCalculate(){
-
-    let total = $( '#addUpTotal' );
-    total.empty();
-    total.append();
-    console.log('jQuery is ready to go!');
-}
-
+//submit employee info function
 function submitEEInfo(){
     //create employee object framework
     const employeeObject = {
@@ -24,31 +19,44 @@ function submitEEInfo(){
         jobTitle: $( '#eeJobTitle' ).val(),
         annualSalary: $( '#eeAnnualSalary' ).val()
     }
-    wages += $( '#eeAnnualSalary' ).val();
+    //starts wage calculation
+    readyToCalculate();
+    //puts our created object in the array
     employeeInfo.push( employeeObject );
+    //starts our display function
     displayEEInfo( employeeInfo );
-    gray();
 }
 
+//display function
 function displayEEInfo(){
+    //finds location for the employ information output
     let eeOut = $( '#eeOutput' );
     eeOut.empty();
+    //gets employee info from the array and prints in in the location above
     for( let i = 0; i < employeeInfo.length; i++){
-        const employeeOut = `<li>${ employeeInfo[i].firstName },
-            ${ employeeInfo[i].lastName }: 
-            ${ employeeInfo[i].idNumber }:
+        const employeeOut = `<li>${ employeeInfo[i].firstName }
+            ${ employeeInfo[i].lastName }
+            # ${ employeeInfo[i].idNumber }
             ${ employeeInfo[i].jobTitle }
-            ${ employeeInfo[i].annualSalary }</li>`;
+            $ ${ employeeInfo[i].annualSalary }</li>`;
         eeOut.append( employeeOut );
-    }
-    
+    }    
 }
 
-function gray() {
-    let sum = 0
-    console.log( sum += wages );
-    
+//calculates the monthly amount of wages
+function readyToCalculate(){
+    //grabs the inputted wages
+    let newWage = $( '#eeAnnualSalary' ).val();
+    let wage = Number( newWage );
+    //adds the wages up and divides it into monthly amounts
+    totalAmount += wage;
+    monthlyAmount = totalAmount/12;
+    //outputs the monthly amount
+    let total = $( '#addUpTotal' );
+    total.empty();
+    total.append( monthlyAmount );
 }
+
 //The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
 
 //A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. 
